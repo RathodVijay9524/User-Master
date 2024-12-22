7f1ebb9528de5fcaebcb51dfae08bda6252d74e8
vijay <omvijay44@gmail.com>
2024-12-22 08:46:33 +0530

    first commit

---
+++ src/main/java/com/vijay/User_Master/UserMasterApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.User_Master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMasterApplication.class, args);
+	}
+
+}
