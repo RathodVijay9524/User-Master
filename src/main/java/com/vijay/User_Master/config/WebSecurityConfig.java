02b578c65e7e6d37e52a95e7fbc1936cdb8e65d5
vijay <omvijay44@gmail.com>
2024-12-22 10:46:08 +0530

    inmemory object create for loging

--- src/main/java/com/vijay/User_Master/config/WebSecurityConfig.java
+++ src/main/java/com/vijay/User_Master/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.vijay.User_Master.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -15,9 +20,22 @@ public class WebSecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
-                   request -> request.anyRequest().authenticated()
-        ).formLogin(Customizer.withDefaults());
+                        request -> request.anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails vijay= User.withUsername("vijay")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails ajay= User.withUsername("ajay")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(vijay,ajay);
     }
 }
